9a4691341da4c5f113dea53fed0bea79ce8531ff
DiegoKraenau <diegokraenau@gmail.com>
2019-09-22 14:04:46 -0500

    Agregado de clase Person

---
+++ RapiSolverApp/src/main/java/edu/rapisolver/rapisolverApp/RapiSolverAppApplication.java
@@ -0,0 +1,13 @@
+package edu.rapisolver.rapisolverApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapiSolverAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapiSolverAppApplication.class, args);
+	}
+
+}
